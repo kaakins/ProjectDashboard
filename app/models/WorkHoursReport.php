d54d67aa6fd88d7efaa33227f245b02bad33a511
kaakins <kaakins@indiana.edu>
2018-10-14 21:29:39 -0400

    created api and model for workHours and made them required for common.php

---
+++ app/models/WorkHoursReport.php
@@ -0,0 +1,29 @@
+<?php
+
+class WorkHoursReport
+{
+  public static function fetchByProjectId(int $projectId) {
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+
+    $sql = 'SELECT DATE(start_date) AS date,
+              SUM(hours) AS hours
+            FROM Work, Tasks
+            WHERE Work.task_id = Tasks.id
+              AND Tasks.project_id = ?
+            GROUP BY DATE(start_date)
+            ORDER BY date';
+
+    $statement = $db->prepare($sql);
+    $success = $statement->execute(
+        [$projectId]
+    );
+
+    if (!$success) {
+      header('500 Server Error');
+      print_r($statement->errorInfo());
+      exit;
+    }
+
+    return $statement->fetchAll(PDO::FETCH_ASSOC);
+  }
+}
